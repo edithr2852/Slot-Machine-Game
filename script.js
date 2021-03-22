f74d23594ea6fe4e4dd24b0c0ba44bd1d75bac52
Edith Rodriguez <edithrodriguez@Ediths-MacBook-Air.local>
2021-03-22 00:10:06 -0700

    added pseudo code to js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log('Connected');
+/*----- constants -----*/
+//the status of the game set to null, results set to null, winning choices (you win the lottery game is over or you bet and bet until you lose all of your money), start button, the initial betting amount, different images (these will be the different outcomes that the slot machine will randomize.), the different section of the slot machine. 
+
+
+
+/*----- app's state (variables) -----*/
+//the results, the betting amount as you lose money
+
+
+
+/*----- cached element references -----*/
+//html elements that will be manipulated - the different images that will get randomized.
+
+
+
+
+/*----- event listeners -----*/
+// will add event listener to the "button" to spin the slot machine
+
+
+
+
+/*----- functions -----*/
+//function that will create random slot outcomes
+//initialize function to start game (which will then prompt you to place a bet then spin then win or lose). 
+//function to place bet that will deduct from the "money" you have- if you lose and function that will give you money if you win.
+//function so you can spin-
+//Winner function (did player win the lottery yes or no? if no keep going until you win or lose all your money.)
+//render function for any changes I may implement in my slot game (for ex: the balance of the player)
