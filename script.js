1ad780ec51098a7cb1c39b8447351053998e84cc
Edith Rodriguez <edithrodriguez@Ediths-MacBook-Air.local>
2021-03-24 09:17:55 -0700

    I was able to update my Javascript. I got my randomizer function to work.

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 /*----- constants -----*/
 //the status of the game set to null, results set to null, winning choices (you win the lottery game is over or you bet and bet until you lose all of your money), start button, the initial betting amount, different images (these will be the different outcomes that the slot machine will randomize.), the different section of the slot machine. 
 
-const slotSymbols = {
-    firstSlot: 'img/Bell.jpeg',
-    secondSlot: 'img/Bomb.jpeg',
-    thirdSlot: 'img/Cherry.jpeg',
-    fourthSlot: 'img/Clover.jpeg',
-    fifthSlot: 'img/Diamond.jpeg',
-    sixthSlot: 'img/Dragon.jpeg',
-    seventhSlot: 'img/Heart.jpeg',
-    eighthSlot: 'img/Horseshoe.jpeg',
-    ninthSlot: 'img/Jackpot.jpeg',
-    tenthSlot: 'img/Moneybag.jpeg',
-    eleventhSlot: 'img/Seven.jpeg',
-    twelthSlot: 'img/Strawberry.jpeg', 
+let slotSymbols = {
+   imgOne: 'img/Bell.jpeg', 
+   imgTwo: 'img/Bomb.jpeg', 
+   imgThree: 'img/Cherry.jpeg', 
+   imgFour: 'img/Clover.jpeg', 
+   imgFive: 'img/Diamond.jpeg', 
+   imgSix: 'img/Dragon.jpeg', 
+   imgSeven: 'img/Heart.jpeg', 
+   imgEight: 'img/Horseshoe.jpeg', 
+   imgNine: 'img/Jackpot.jpeg', 
+   imgTen: 'img/Jackpot.jpeg', 
+   imgEleven: 'img/Seven.jpeg', 
+   imgTwelve: 'img/Strawberry.jpeg'
 }
 
 
@@ -22,9 +22,9 @@ const slotSymbols = {
 
 
 
-const winAudio = new Audio('https://soundcloud.com/professional-sound-effects-group/winning-slot-machine');
-const spinAudio = new Audio( 'https://soundcloud.com/lavs-test/slot-machine-reel-spin-test' );
-const backgroundAudio = new Audio ('https://soundcloud.com/search?q=retro%20casino%20slot%20machine');
+const winAudio = new Audio('file:///Users/edithrodriguez/Music/Music/Media.localized/Unknown%20Artist/Unknown%20Album/slot-machine-sound-effect.mp3');
+const spinAudio = new Audio('file:///Users/edithrodriguez/Music/Music/Media.localized/SFX%20Producer/Unknown%20Album/Casino%20Slot%20Machine%2017.mp3');
+
 
 /*----- app's state (variables) -----*/
 //the results, the betting amount as you lose money
@@ -35,75 +35,80 @@ let spinResult, lotteryWinner;
 //html elements that will be manipulated - the different images that will get randomized.
 
 const gameResults = document.querySelector('h2');
-const firstDiv = document.getElementById('#first')
-const secondDiv = document.getElementById('#second')
-const thirdDiv = document.getElementById('#third')
+const firstDiv = document.getElementById('#first');
+const secondDiv = document.getElementById('#second');
+const thirdDiv = document.getElementById('#third');
 
-// const winningCombo = [ 
-//     [firstDiv = 'img/Jackpot.jpeg'],
-//     [secondDiv = 'img/Jackpot.jpeg'],
-//     [thirdDiv = 'img/Jackpot.jpeg'],
-// ];
+let slotOne = getRandomSymbols(Object.values(slotSymbols));
+console.log(slotOne)
+let slotTwo = getRandomSymbols(Object.values(slotSymbols));
+console.log(slotTwo)
+let slotThree = getRandomSymbols(Object.values(slotSymbols));
+console.log(slotThree)
 
-// console.log(winningCombo);
 
 /*----- event listeners -----*/
 // will add event listener to the "button" to spin the slot machine
 
-// document.querySelector('div button').addEventListener('click', spin) => {
-//     console.log('clicked'); };
-
-document.querySelector('div button').addEventListener("click", spin() => {
-        console.log("You knocked?");
-      });
+document.querySelector('div button').addEventListener("click", spin);
 
 /*----- functions -----*/
-//function that will create random slot outcomes
-//initialize function to start game (which will then prompt you to place a bet then spin then win or lose). 
-//function to place bet that will deduct from the "money" you have- if you lose and function that will give you money if you win.
-//function so you can spin-
-//Winner function (did player win the lottery yes or no? if no keep going until you win or lose all your money.)
-//render function for any changes I may implement in my slot game (for ex: the balance of the player)
-//function that will provide the spin when to start or how long to delay. 
-
 
 function initialize() {
     //start message // 
     spinResult = {
-    firstDiv: 'img/Bell.jpeg',
-    secondDiv: 'img/Bomb.jpeg',
-    thirdDiv: 'img/Cherry.jpeg',
+    firstDiv: '',
+    secondDiv: '',
+    thirdDiv: '',
     }
     lotteryWinner = null 
-    render();
+    spin();
+    
 }
 
 function getRandomSymbols() {
-    return Math.floor(Math.random() * 12);
+    return Math.floor(Math.random() * 12)
 }
 
 function spin() {
     spinAudio.play();
-    spinner = slotSymbols[getRandomSymbols()];
-    winner = getWinner(); 
-    // render()
+    slotOne = getRandomSymbols(Object.values(slotSymbols));
+console.log(slotOne)
+    slotTwo = getRandomSymbols(Object.values(slotSymbols));
+console.log(slotTwo)
+    slotThree = getRandomSymbols(Object.values(slotSymbols));
+console.log(slotThree)
+    render()
 }
+
+
 
 function getWinner () {
     if(firstDiv === 'img/Jackpot.jpeg' && secondDiv === 'img/Jackpot.jpeg' && thirdDiv === 'img/Jackpot.jpeg') {
+        winAudio.play();
         return gameResults; 
     } else {
-        return gameResults;
+        return spinResult;
     }
     spin()
 }
 
 
-function render() {
-    //add an event listener that will 
-    firstDiv.innerHTML = 'getRandomSymbols';
-    secondDiv.innerHTML = 'getRandomSymbols';
-    thirdDiv.innerHTML = 'getRandomSymbols';
+function render(getRandomSymbols) {
+firstDiv.style.backgroundImage = `url(${slotSymbols[slotOne].imgOne})`;
+SecondDiv.style.backgroundImage = `url(${slotSymbols[imgTwo].imgTwo})`;
+ThirdDiv.style.backgroundImage = `url(${slotSymbols[imgThree].imgThree})`;
+
+gameResults.innerText = 'You won the lottery!'
+
 }
+
+
+//    firstDiv.addEventListener('animationend', () => {
+//    });
+//    secondtDiv.addEventListener('animationend', () => {
+//     });
+//    thirdDiv.addEventListener('animationend', () => {
+//     }); 
 
 initialize();
